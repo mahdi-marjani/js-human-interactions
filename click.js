b8ab98345e553f7a618a29baeb2fd638aa0d8890
Mahdi <mahdi.marjani@outlook.com>
2024-07-24 15:18:52 +0330

    Update click.js

--- click.js
+++ click.js
@@ -1,20 +1,23 @@
-simulateMouseEvent = function(element, eventName, coordX, coordY) {
+function simulateClick(element) {
+  function simulateMouseEvent(element, eventName, coordX, coordY) {
     element.dispatchEvent(new MouseEvent(eventName, {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-        clientX: coordX,
-        clientY: coordY,
-        button: 0
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      clientX: coordX,
+      clientY: coordY,
+      button: 0
     }));
-};
-  
-element = document.querySelector(''); // Element to click
+  };
 
-box = element.getBoundingClientRect(),
+  box = element.getBoundingClientRect(),
     coordX = box.left + (box.right - box.left) / 2,
     coordY = box.top + (box.bottom - box.top) / 2;
 
-simulateMouseEvent (element, "mousedown", coordX, coordY);
-simulateMouseEvent (element, "mouseup", coordX, coordY);
-simulateMouseEvent (element, "click", coordX, coordY);
+  simulateMouseEvent(element, "mousedown", coordX, coordY);
+  simulateMouseEvent(element, "mouseup", coordX, coordY);
+  simulateMouseEvent(element, "click", coordX, coordY);
+};
+  
+element = document.querySelector(''); // Element to click
+simulateClick(element)
